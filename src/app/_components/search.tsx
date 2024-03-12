484fd2fb7604ca553e2813291c110ef5e3b4accd
Sundar Jayabose <jayabose@gmail.com>
2024-03-12 17:53:51 -0400

    feat: autocomplete integrated into search

--- src/app/_components/search.tsx
+++ src/app/_components/search.tsx
@@ -6,9 +6,10 @@ import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchBar from './SearchBar';
 
 type SearchProps = {
-  onSearch: (event: Event) => Promise<void>,
+  onSearch: (event: Event) => Promise<void>;
   onToggleTempUnits: () => void;
 };
 
@@ -18,30 +19,15 @@ export default function Search({ onSearch, onToggleTempUnits }: SearchProps) {
   return (
     <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
       <Box component="div" sx={{ display: 'flex', alignItems: 'center' }}>
-      <form onSubmit={onSearch} style={{ flex: 1 }}>
-        <TextField
-          name="search-term"
-          variant="outlined"
-          label="Search for a place..."
-          helperText="Enter name or lattitude and longitude coordinates (e.g. 40.71 74.0)"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-          margin="dense"
-          fullWidth
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton aria-label="search" type="submit">
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </form>
-      <IconButton onClick={onToggleTempUnits}>
-        <MoreVertIcon />
-      </IconButton>
+        <form onSubmit={onSearch} style={{ flex: 1 }}>
+          <SearchBar/>
+          <IconButton aria-label="search" type="submit">
+            <SearchIcon />
+          </IconButton>
+        </form>
+        <IconButton onClick={onToggleTempUnits}>
+          <MoreVertIcon />
+        </IconButton>
       </Box>
     </Box>
   );
